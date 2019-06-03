d2b38458b774d211ba98c74f26fb41fe50856c4d
Emma Qian <qianhanmin99@gmail.com>
2019-06-02 23:25:57 -0700

    complete plot error from different files

--- plot_errors.py
+++ plot_errors.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 path = 'errors'
 all_files = glob.glob(path + "/*.csv")
@@ -9,13 +10,20 @@ all_files = glob.glob(path + "/*.csv")
 errors = []
 
 for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=None)
-    errors.append(df)
+    data = csv.reader(open(filename, 'r'), delimiter=",")
+    column1, column2 = [], []
 
-frame = pd.concat(errors, axis=0, ignore_index=True)
-x = list(range(len(frame.iloc[:,0])))
-for data in frame.iloc[:,0]:
-    print(data)
-    plt.plot(x, data, color='olive')
-for data in frame.iloc[1::2, :]:
-    plt.plot(x, data, color='skyblue')
+    for row in data:
+        column1.extend(row[0])
+        column2.extend(row[1])
+
+    print(column1)
+    print(column2)
+
+# frame = pd.concat(errors, axis=0, ignore_index=True)
+# x = list(range(len(frame.iloc[:,0])))
+# for data in frame.iloc[:,0]:
+#     print(data)
+#     plt.plot(x, data, color='olive')
+# for data in frame.iloc[1::2, :]:
+#     plt.plot(x, data, color='skyblue')
